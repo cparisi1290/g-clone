7b999331492da2626f3e44169c6174c5e6aba90f
Cassandra <cparisi1290@gmail.com>
2021-07-27 16:13:10 -0500

    import link from react router dom, add links to class name headers

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import './Home.css'
+import { Link } from 'react-router-dom'
 
 function Home() {
     return (
@@ -7,7 +8,11 @@ function Home() {
             <h1>Homepage</h1>
             <div className="home-header">
                 <div className="home-header-left"></div>
+                    <Link to="about">About</Link>
+                    <Link to="store">Store</Link>
                 <div className="home-header-right"></div>
+                    <Link to="gmail">Gmail</Link>
+                    <Link to="images">Images</Link>
             </div>
             <div className="home-body">
 
