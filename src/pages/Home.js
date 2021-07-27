f22cd03a422e6e90639f75b708c3bdb732218ab3
Cassandra <cparisi1290@gmail.com>
2021-07-27 17:39:08 -0500

    add logo image, sudo code for search component

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,21 +1,30 @@
 import React from 'react'
 import './Home.css'
 import { Link } from 'react-router-dom'
+import AppsIcon from '@material-ui/icons/Apps'
+import { Avatar } from '@material-ui/core'
 
 function Home() {
     return (
-        <div>
-            <h1>Homepage</h1>
+        <div className="home">
             <div className="home-header">
-                <div className="home-header-left"></div>
-                    <Link to="about">About</Link>
-                    <Link to="store">Store</Link>
-                <div className="home-header-right"></div>
-                    <Link to="gmail">Gmail</Link>
-                    <Link to="images">Images</Link>
+                <div className="home-headerLeft"></div>
+                    <Link to="/about">About</Link>
+                    <Link to="/store">Store</Link>
+                <div className="home-headerRight"></div>
+                    <Link to="/gmail">Gmail</Link>
+                    <Link to="/images">Images</Link>
+                    <AppsIcon />
+                    <Avatar />
             </div>
             <div className="home-body">
-
+                <img 
+                    src="https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png"
+                    alt=""
+                />
+            </div>
+            <div className="home-inputContainer">
+                {/* Search */}
             </div>
         </div>
     )
