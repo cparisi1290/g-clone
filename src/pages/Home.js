6161b0c1a012569e54da2fb8c80f95f60fb4921d
Cassandra <cparisi1290@gmail.com>
2021-07-27 16:01:36 -0500

    install react-dom-router, install material-ui and icons, create pages and componetns folder, create home component and css page, start html in home, import home into app.js

---
+++ src/pages/Home.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import './Home.css'
+
+function Home() {
+    return (
+        <div>
+            <h1>Homepage</h1>
+            <div className="home-header">
+                <div className="home-header-left"></div>
+                <div className="home-header-right"></div>
+            </div>
+            <div className="home-body">
+
+            </div>
+        </div>
+    )
+}
+
+export default Home
