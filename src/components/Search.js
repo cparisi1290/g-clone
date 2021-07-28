6e7827d5f96e33e2b2ce52a07f92f1ae64bd87c5
Cassandra <cparisi1290@gmail.com>
2021-07-28 14:33:43 -0500

    pulled data from react context api

--- src/components/Search.js
+++ src/components/Search.js
@@ -3,9 +3,12 @@ import './Search.css'
 import SearchIcon from '@material-ui/icons/Search';
 import MicIcon from '@material-ui/icons/Mic';
 import { Button } from '@material-ui/core';
-import { useHistory } from 'react-router-dom'
+import { useHistory } from 'react-router-dom';
+import { useStateValue } from '../StateProvider';
+import { actionTypes } from '../reducer';
 
 function Search({ hideButtons = false }) {
+    const [{}, dispatch] = useStateValue();
     const [input, setInput] = useState("");
     const history = useHistory();
 
@@ -14,6 +17,11 @@ function Search({ hideButtons = false }) {
         e.preventDefault();
         
         console.log("You hit the search button >>", input)
+
+        dispatch({
+            type: actionTypes.SET_SEARCH_TERM,
+            term: input
+        })
 
         history.push('/search')
 
