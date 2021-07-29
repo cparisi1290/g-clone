3410c808f69d86f03bc2037886ed583cd3bb2d44
Cassandra <cparisi1290@gmail.com>
2021-07-29 05:46:24 -0500

    use custom hook, set up googleapi and comment out for production, insert response data from one googleapi pull, import icons for search options

---
+++ src/components/SearchPage.js
@@ -0,0 +1,71 @@
+import React from "react";
+import "./SearchPage.css";
+import { useStateValue } from "../StateProvider";
+import useGoogleSearch from "../useGoogleSearch";
+import Response from "../response";
+import { Link } from "react-router-dom";
+import Search from "./Search";
+import SearchIcon from "@material-ui/icons/Search";
+import DescriptionIcon from "@material-ui/icons/Description";
+import ImageIcon from "@material-ui/icons/Image";
+import LocalOfferIcon from "@material-ui/icons/LocalOffer";
+import RoomIcon from "@material-ui/icons/Room";
+import MoreVertIcon from "@material-ui/icons/MoreVert";
+
+function SearchPage() {
+  const [{ term }, dispatch] = useStateValue();
+
+  // LIVE API CALL
+  // const { data } = useGoogleSearch(term);
+
+  const data = Response;
+
+  console.log(data);
+  return (
+    <div className="searchPage">
+      <div className="searchPage-header">
+        <Link to="/">
+          <img
+            src="https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png"
+            alt=""
+          />
+        </Link>
+        <div className="search-headerBody">
+          <Search hideButtons />
+          <div className="searchPage-options">
+            <div className="searchPage-optionsLeft">
+              <div className="searchPage-option">
+                <SearchIcon />
+                <Link to="/all">All</Link>
+              </div>
+              <div className="searchPage-option">
+                <DescriptionIcon />
+                <Link to="/news">News</Link>
+              </div>
+              <div className="searchPage-option">
+                <ImageIcon />
+                <Link to="/images">Images</Link>
+              </div>
+              <div className="searchPage-option">
+                <LocalOfferIcon />
+                <Link to="/shopping">shopping</Link>
+              </div>
+              <div className="searchPage-option">
+                <RoomIcon />
+                <Link to="/maps">maps</Link>
+              </div>
+              <div className="searchPage-option">
+                <MoreVertIcon />
+                <Link to="/more">more</Link>
+              </div>
+            </div>
+            <div className="searchPage-optionsRight"></div>
+          </div>
+        </div>
+      </div>
+      <div className="searchPage-results"></div>
+    </div>
+  );
+}
+
+export default SearchPage;
